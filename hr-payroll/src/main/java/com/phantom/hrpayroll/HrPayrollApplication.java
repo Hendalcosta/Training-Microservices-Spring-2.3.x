8aafd5e3e1f123928c374676e8a21a2857a30abc
Henderson <hendalcosta@gmail.com>
2022-10-16 12:24:33 -0300

    Feign Client

--- hr-payroll/src/main/java/com/phantom/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/phantom/hrpayroll/HrPayrollApplication.java
@@ -2,7 +2,9 @@ package com.phantom.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients //Anotation cloud que habilita o projeto a trabalhar com o FeignClient
 @SpringBootApplication
 public class HrPayrollApplication {
 
--- hr-payroll/src/main/java/com/phantom/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/phantom/hrpayroll/services/PaymentService.java
@@ -1,30 +1,21 @@
 package com.phantom.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.phantom.hrpayroll.entities.Payment;
 import com.phantom.hrpayroll.entities.Worker;
+import com.phantom.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")//associa o valor da propriedade hr-worker.host no application.properties para a variavel workerHost
-	private String workerHost;//armazena o endereço e porta do hr-worker na variável workerHost
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();//cria uma map para armazenar os parametros do método .getForObject
-		uriVariables.put("id", ""+workerId);//aspas + workerId para converter o tipo de worker ID (long) para o tipo do map (string)
 		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);//argumentos são: URL composta pelo endereço de horkhost + ID, o tipo de objeto requisitado e a s variaveis no Map de Uri
+		Worker worker = workerFeignClient.findById(workerId).getBody();//.getBody() serve para acessar o corpo da resposta que é do tipo worker. Sem ele vai retornar o tipo ResponseEntity 
 		return new Payment(worker.getName(),worker.getDailyIncome(), days);
 	}
 }
